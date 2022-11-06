dea74a0897350b8292f13a791f73c94f512d6250
hama-hiroki <trashman1214@icloud.com>
2022-11-06 14:39:41 +0900

    モデルテスト実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :items
+         has_many :orders
+     
+       validates :nickname, presence:true
+       validates :last_name, presence:true, format: { with: /\A[ぁ-んァ-ン一-龥々]/}
+       validates :first_name, presence:true, format: { with: /\A[ぁ-んァ-ン一-龥々]/}
+       validates :last_name_kana, presence:true, format: { with: /\A[ァ-ヶー－]+\z/}
+       validates :first_name_kana, presence:true, format: { with: /\A[ァ-ヶー－]+\z/}
+       validates :birthday, presence:true
+       validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+       validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
