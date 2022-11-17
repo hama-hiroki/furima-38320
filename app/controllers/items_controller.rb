ec781dcf2c02f16b5f2202c2b889107933ef9df3
hama-hiroki <trashman1214@icloud.com>
2022-11-17 17:14:51 +0900

    商品一覧表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    #@items = Item.all
+    @items = Item.all
   end
 
   def new
@@ -17,6 +17,9 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+    
+    def show
+    end
 
   private
 
