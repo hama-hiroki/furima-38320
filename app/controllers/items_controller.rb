932b7beec95dd8f91b2564d2fe05db08178df08f
hama-hiroki <trashman1214@icloud.com>
2022-11-17 22:11:08 +0900

    商品情報編集機能 実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,14 +12,27 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to item_path(@items.id)
     else
-      render :new
+      render :show
     end
   end
     
   def show
     @items = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id]) 
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
 
@@ -31,4 +44,11 @@ class ItemsController < ApplicationController
                   :shipping_charge_id, :shipping_area_id, :estimated_shipping_date_id,
                   :price).merge(user_id: current_user.id)
   end
+  
+  def autheniticate_user
+    if current_user==nil
+      redirect_to("/login")
+    end
+  end
+
 end
