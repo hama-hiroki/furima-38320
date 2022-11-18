32c551adc2ff2603bcd1d9e49609acf1e07cf299
hama-hiroki <trashman1214@icloud.com>
2022-11-18 19:21:09 +0900

    URLリンク時の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:show,:edit]
 
   def index
@@ -48,6 +49,10 @@ class ItemsController < ApplicationController
   end
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
   end
   
   def autheniticate_user
